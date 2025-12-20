c59751f8001969ed2b9f1a5ae52f8f7773526603
trihfan <thibault.cardaire@gmail.com>
2025-12-20 10:30:01 +0100

    update script

--- hdmi_detect/detect_hdmi_input.py
+++ hdmi_detect/detect_hdmi_input.py
@@ -11,10 +11,13 @@ while True:
     try:
         r = requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22current-state%22,%22subcommand%22:%22average-color%22,%22instance%22:0%7D", timeout=1)
         data = r.json()
-        print(data)
-        signal = data.get("signal", False)
+        red = data["info"].get("red", 0)
+        green = data["info"].get("green", 0)
+        blue = data["info"].get("blue", 0)
+        print(red + green + blue)
+        has_input = (red + green + blue) > 0
 
-        GPIO.output(GPIO_PIN, GPIO.HIGH if signal else GPIO.LOW)
+        GPIO.output(GPIO_PIN, GPIO.HIGH if has_input else GPIO.LOW)
 
     except:
         GPIO.output(GPIO_PIN, GPIO.LOW)
