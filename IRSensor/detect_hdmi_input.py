86af67f4469605b5bd2def0e84d8bbeab29e1755
trihfan <thibault.cardaire@gmail.com>
2025-12-27 10:00:41 +0100

    change delay

--- IRSensor/detect_hdmi_input.py
+++ IRSensor/detect_hdmi_input.py
@@ -2,7 +2,7 @@ import requests
 import RPi.GPIO as GPIO
 import time
 
-THRESHOLD = 33
+THRESHOLD = 33 # With current config, min for each color is 11
 GPIO_PIN = 17
 
 GPIO.setmode(GPIO.BCM)
@@ -15,23 +15,25 @@ MIN_DELAY = 10
 
 while True:
     try:
-        r0 = requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22current-state%22,%22subcommand%22:%22average-color%22,%22instance%22:0%7D", timeout=1)
-        r1 = requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22current-state%22,%22subcommand%22:%22average-color%22,%22instance%22:1%7D", timeout=1)
+        # Get ambilight color state
+        request_ambilight_led_color = requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22current-state%22,%22subcommand%22:%22average-color%22,%22instance%22:0%7D", timeout=1)
 
-        data0 = r0.json()
-        data1 = r1.json()
+        # Get virtual color state, works even when ambilight is disabled
+        request_virtual_led_color = requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22current-state%22,%22subcommand%22:%22average-color%22,%22instance%22:1%7D", timeout=1)
 
-        total0 = data0["info"].get("red", 0) + data0["info"].get("green", 0) + data0["info"].get("blue", 0)
-        total1 = data1["info"].get("red", 0) + data1["info"].get("green", 0) + data1["info"].get("blue", 0)
+        data_ambilight_led_color = request_ambilight_led_color.json()
+        data_virtual_led_color = request_virtual_led_color.json()
 
-        has_input0 = total0 > THRESHOLD
-        has_input1 = total1 > THRESHOLD
+        total_ambilight_led_color = data_ambilight_led_color["info"].get("red", 0) + data_ambilight_led_color["info"].get("green", 0) + data_ambilight_led_color["info"].get("blue", 0)
+        total_virtual_led_color = data_virtual_led_color["info"].get("red", 0) + data_virtual_led_color["info"].get("green", 0) + data_virtual_led_color["info"].get("blue", 0)
+
+        has_input_virtual_led_color = total_virtual_led_color > THRESHOLD
         
-        print(f"received {total1}, threshold is {THRESHOLD}")
+        print(f"received {total_virtual_led_color}, threshold is {THRESHOLD}")
 
-        if (has_input1 != wanted_state):
+        if (has_input_virtual_led_color != wanted_state):
             start_changed = time.time()
-            wanted_state = has_input1
+            wanted_state = has_input_virtual_led_color
 
         print(f"current_state {current_state}, wanted_state {wanted_state}")
 
@@ -43,12 +45,12 @@ while True:
             GPIO.output(GPIO_PIN, GPIO.HIGH if current_state else GPIO.LOW)
 
         # Turn led on or off
-        if (total0 == 0 and current_state):
+        if (total_ambilight_led_color == 0 and current_state):
             requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:true%7D%7D")
-        if (total0 > 0 and not current_state):
+        if (total_ambilight_led_color > 0 and not current_state):
             requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:false%7D%7D")
 
     except:
         GPIO.output(GPIO_PIN, GPIO.LOW)
 
-    time.sleep(0.5)
+    time.sleep(0.2)
