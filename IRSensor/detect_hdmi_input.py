238888510ee4a49c254bb956fad1152368b3342b
trihfan <thibault.cardaire@gmail.com>
2025-12-28 23:08:50 +0100

    put back delay

--- IRSensor/detect_hdmi_input.py
+++ IRSensor/detect_hdmi_input.py
@@ -7,7 +7,11 @@ GPIO_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.OUT)
 
-#GPIO.output(GPIO_PIN, GPIO.LOW)
+# Add delay when turning off to rpevent false positives
+current_state = False
+wanted_state = False
+start_changed = 0
+MIN_DELAY = 0
 
 while True:
     try:
@@ -18,13 +22,26 @@ while True:
             if priority["componentId"] == "VIDEOGRABBER":
                 active = priority.get("active", False)
                 print(f"state is {active}")
-                GPIO.output(GPIO_PIN, GPIO.HIGH if active else GPIO.LOW)
 
-                # Turn led on or off
-                if active:
-                    requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:true%7D%7D")
-                else:
-                    requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:false%7D%7D")
+                if (active != wanted_state):
+                    start_changed = time.time()
+                    wanted_state = active
+
+                can_change = (time.time() - start_changed) > MIN_DELAY
+
+                 # Turn on instantaneously but off could be a false positive
+                if (wanted_state != current_state and (wanted_state or can_change)):
+                    current_state = wanted_state
+                    GPIO.output(GPIO_PIN, GPIO.HIGH if current_state else GPIO.LOW)
+
+                #GPIO.output(GPIO_PIN, GPIO.HIGH if active else GPIO.LOW)
+
+                    # Turn led on or off
+                    if active:
+                        requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:true%7D%7D")
+                    else:
+                        requests.get("http://localhost:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22LEDDEVICE%22,%22state%22:false%7D%7D")
+
                 break
 
     except:
--- IRSensor/irsensor/irsensor.ino
+++ IRSensor/irsensor/irsensor.ino
@@ -5,7 +5,7 @@
 
 // Params
 #define ENABLE_SERIAL 1
-#define SLEEP_TIME 500
+#define SLEEP_TIME 1000
 
 // Global
 unsigned long time;
@@ -37,17 +37,16 @@ uint16_t tv_raw_data[TV_RAW_DATA_LEN] =
 	450, 2050, 450, 1000, 500, 2000, 450, 1000, 
 	500, 2000, 500, 1000
 };
-#define TV_LIGHT_THRESHOLD 10
-#define TV_POWER_ON_DELAY 10000
-#define TV_POWER_OFF_DELAY 10000
-#define TV_FALSE_ON_DELAY 20000
+#define TV_LIGHT_THRESHOLD 0.1
+#define TV_POWER_DELAY 3000
 
 LightDependentResistor photocell_tv(A1, 3000, LightDependentResistor::GL5528);
 IRsendRaw sender_tv;
 float light_intensity_tv;
 bool tv_is_on = false;
 unsigned long tv_busy_until = 0;
-unsigned long tv_turn_on_at = 0;
+
+bool code_sent = false;
 
 // Functions
 void do_tv();
@@ -75,7 +74,10 @@ void loop()
   do_home_cinema();
 
   // 3 - Handle tv
-  do_tv();
+  if (!code_sent)
+  {
+    do_tv();
+  }
 
   // 4 - Output and sleep
 #ifdef ENABLE_SERIAL
@@ -99,54 +101,23 @@ void do_tv()
 {
   // Get tv power state with photocell
   light_intensity_tv = photocell_tv.getCurrentLux();
-  bool tv_is_on_now = light_intensity_tv > TV_LIGHT_THRESHOLD;
+  tv_is_on = light_intensity_tv < TV_LIGHT_THRESHOLD;
 
-  // Save the time when the tv has turned on
-  if (tv_is_on_now && !tv_is_on)
+  // Update tv power state
+  if (input_is_on != tv_is_on && tv_busy_until < time)
   {
-    tv_turn_on_at = time;
-  }
-  tv_is_on = tv_is_on_now;
-  
-  // Can we hande the state
-  if (tv_busy_until >= time) 
-  {
-    return;
-  }
-
-  bool send_tv_signal = false;
-
-  if (input_is_on)
-  {
-    if (!tv_is_on)
-    {
-      send_tv_signal = true;
-      tv_busy_until = time + TV_POWER_ON_DELAY;
-    }
-  }
-  else
-  {
-    if (tv_is_on)
-    {
-      if (time > (tv_turn_on_at + TV_FALSE_ON_DELAY))
-      {
-        send_tv_signal = true;
-        tv_busy_until = time + TV_POWER_OFF_DELAY;
-      }
-    }
-  }
-
-  if (send_tv_signal)
-  {
+    tv_busy_until = time + TV_POWER_DELAY;
     sender_tv.send(tv_raw_data, TV_RAW_DATA_LEN, 36);
 #ifdef ENABLE_SERIAL
-      Serial.print("tv power on sent\n");
+    Serial.print("tv power on sent\n");
 #endif
   }
 }
 
 void do_home_cinema()
 {
+  code_sent = false;
+
   // Get home cinema power state with photocell
   light_intensity_home_cinema = photocell_home_cinema.getCurrentLux();
   home_cinema_is_on = light_intensity_home_cinema > HOME_CINEMA_LIGHT_THRESHOLD;
@@ -156,6 +127,7 @@ void do_home_cinema()
   {
     sender_home_cinema.send(HOME_CINEMA_KEY_POWER, 15);
     home_cinema_busy_until = time + HOME_CINEMA_POWER_DELAY;
+    code_sent = true;
 #ifdef ENABLE_SERIAL
     Serial.print("home cinema power on sent\n");
 #endif
